358e32a9a907f2ee73298062cd30956b1a546356
priyanshu1998 <priyanshutirkey1998@gmail.com>
2019-09-01 07:26:31 +0530

    updated guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+let file_cnt=0
+
+function get_the_counts(){
+    for item in ./*
+        do
+            if [[ -f $item ]]
+            then
+                file_cnt=$(($file_cnt+1))
+            fi
+        done
+}
+
+
+get_the_counts
+echo "${file_cnt}"
+
+let usr_input=-1
+
+while [[ $usr_input -ne $file_cnt ]]
+do
+    read usr_input
+
+    if [[ $usr_input -eq $file_cnt ]]
+    then
+        echo "correct guess!!!"
+    else
+        if [[ $usr_input -lt $file_cnt ]]
+        then
+            echo "the correct count is greater"
+        else
+            echo "the correct count is lesser"
+        fi
+    fi
+done
+
+
+
